cc9a8a1a5a898cad5c2497b42e0fc8d7bd4cb07d
Srikanth Mohan <srikanth.mohan.98@gmail.com>
2026-02-26 17:55:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+getSumBtn.addEventListener("click", getSum);
+function getSum(){
+	let totalsum=0;
+ const prices=document.querySelectorAll(".price");
+	prices.forEach(price=>{
+		totalsum= totalsum+Number(price.textContent);
+	});
+	const table=document.getElementsByTagName("table")[0];
+	const newRow=document.createElement("tr");
+	const newCell=document.createElement("td");
 
-const getSum = () => {
-//Add your code here
-  
+	newCell.textContent=totalsum;
+	newRow.appendChild(newCell);
+	table.appendChild(newRow);
 };
 
-getSumBtn.addEventListener("click", getSum);
+
+
 
